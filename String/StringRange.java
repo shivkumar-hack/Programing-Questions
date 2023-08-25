2a77ed6613ca2e6150536d94090ce83ea08fe3d4
shiv kumar singh <117354199+shivkumar-hack@users.noreply.github.com>
2023-08-25 21:26:43 +0530

    Update StringRange.java

--- String/StringRange.java
+++ String/StringRange.java
@@ -1,8 +1,16 @@
 package String;
-
+import java.util.*;
 public class StringRange {
     public static void main(String[] args) {
-        String s1 = "helloworld";
-        System.out.println(s1.substring(3,7));
+        Scanner in = new Scanner (System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+
     }
 }
+// ouput - l o w o 
+//         h e l l o w o r l d 
+//         0 1 2 3 4 5 6 7 8 9 
+                  

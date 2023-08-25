2367f32181d84fda92581a0c70699cd76053e1fa
shiv kumar singh <117354199+shivkumar-hack@users.noreply.github.com>
2023-08-25 21:21:59 +0530

    Add files via upload

---
+++ String/StringRange.java
@@ -0,0 +1,8 @@
+package String;
+
+public class StringRange {
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        System.out.println(s1.substring(3,7));
+    }
+}
